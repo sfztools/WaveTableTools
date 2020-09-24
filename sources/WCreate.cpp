a2694b62768601c697c73973faa8ccb7b6f292f3
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-09-24 20:12:48 +0200

    WCreate: Add a waveform display

--- sources/WCreate.cpp
+++ sources/WCreate.cpp
@@ -149,6 +149,86 @@ static bool fill_spectrum_in_partials_mode(const char *text, kiss_fft_cpx *spect
     return true;
 }
 
+static void display_wave(const float* data, unsigned size, int rows, int cols)
+{
+    if ((rows & 1) == 0)
+        --rows;
+    if ((cols & 1) == 0)
+        --cols;
+
+    if (rows < 1 || cols < 1)
+        return;
+
+    std::unique_ptr<char[]> text(new char[rows * (cols + 1) + 1]);
+    {
+        char *p = text.get();
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c)
+                *p++ = ' ';
+            *p++ = '\n';
+        }
+        *p++ = '\0';
+    }
+    auto put_char = [&text, rows, cols](int r, int c, char ch) {
+        if (r >= 0 && c >= 0 && r < rows && c < cols)
+            text[r * (cols + 1) + c] = ch;
+    };
+    auto put_hline = [&put_char](int r, int c1, int c2, char ch) {
+        for (int c = c1; c <= c2; ++c)
+            put_char(r, c, ch);
+    };
+    auto put_vline = [&put_char](int r1, int r2, int c, char ch) {
+        for (int r = r1; r <= r2; ++r)
+            put_char(r, c, ch);
+    };
+
+    auto lerp = [data, size](float x) {
+        float index = x * size;
+        unsigned i1 = (unsigned)index % size;
+        unsigned i2 = ((unsigned)index + 1) % size;
+        float mu = index - i1;
+        return (1 - mu) * data[i1] + mu * data[i2];
+    };
+
+
+    // borders
+    put_hline(0, 0, cols - 1, '-');
+    put_hline(rows - 1, 0, cols - 1, '-');
+    put_vline(0, rows - 1, 0, '|');
+    put_vline(0, rows - 1, cols - 1, '|');
+    // corners
+    put_char(0, 0, '+');
+    put_char(0, cols - 1, '+');
+    put_char(rows - 1, 0, '+');
+    put_char(rows - 1, cols - 1, '+');
+    // axes
+    put_hline(rows / 2, 1, cols - 2, '.');
+    put_vline(1, rows - 2, cols / 2, '.');
+
+    // data points
+    float rr = 0.0f;
+    for (int c = 1; c < cols - 1; ++c) {
+        float x = (float)(c - 1) / (float)(cols - 2);
+        float y = lerp(x);
+        float oldrr = rr;
+        rr = 1 + 0.5f * (1 - y) * (rows - 2);
+        put_char((int)std::lround(rr), c, '*');
+        if (c > 1) {
+            int r1 = (int)std::lround(oldrr);
+            int r2 = (int)std::lround(rr);
+            int rinc = (r1 < r2) ? +1 : -1;
+            for (int r = r1; r != r2; r += rinc) {
+                float mu = (r - oldrr) / (rr - oldrr);
+                float midrr = (1 - mu) * oldrr + mu * rr;
+                put_char((int)std::lround(midrr), c, '*');
+            }
+        }
+    }
+
+    //
+    fputs(text.get(), stdout);
+}
+
 int main(int argc, char *argv[])
 {
     if (!parse_args(argc, argv))
@@ -191,11 +271,8 @@ int main(int argc, char *argv[])
     }
 
     //
-    if (0) {
-        for (unsigned i = 0; i < wt.frames; ++i) {
-            printf("%u %e\n", i, wt.data[i]);
-        }
-    }
+    if (1)
+        display_wave(wt.data.get(), wt.frames, 20, 80);
 
     //
     FILE *stream = fopen(output_filename, "wb");
